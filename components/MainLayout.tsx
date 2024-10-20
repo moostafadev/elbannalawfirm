36dc1bace56585599d3360942ff680a1d9b5a0eb
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-10-20 21:57:29 +0300

    fix

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -1,24 +1,23 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Header from "./Header";
 import ScrollToTop from "./ScrollToTop";
-import ClientSideWrapper from "./Loading";
 import ContactBtn from "./ContactBtn";
 import { Toaster } from "./ui/toaster";
 import Footer from "./Footer";
+import Loading from "./Loading";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <Suspense fallback={<Loading />}>
       <Header />
       <main className="overflow-hidden mt-24">
         {children}
         <ScrollToTop />
         <ContactBtn />
         <Toaster />
-        <ClientSideWrapper />
       </main>
       <Footer />
-    </>
+    </Suspense>
   );
 };
 
