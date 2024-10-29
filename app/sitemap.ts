8b0fb743318237331e9b0f42d68af01c50ff937b
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-10-29 15:46:11 +0300

    add sitemap file

---
+++ app/sitemap.ts
@@ -0,0 +1,17 @@
+import { blogsData } from "@/data/blogs";
+import { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const languages = ["ar", "en", "fr"];
+  const sitemapData: MetadataRoute.Sitemap = [];
+
+  blogsData.forEach(({ id }) => {
+    languages.forEach((lang) => {
+      sitemapData.push({
+        url: `https://www.elbannalawfirm.com/${lang}/blog/${id}`,
+      });
+    });
+  });
+
+  return sitemapData;
+}
